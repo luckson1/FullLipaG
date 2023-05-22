4bfa58c49454413682ce3d5af559312fe5dc76c5
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-22 04:58:45 +0300

    feat: get latest rates

--- packages/api/src/router/exchange.ts
+++ packages/api/src/router/exchange.ts
@@ -1,7 +1,7 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { adminProcedure, createTRPCRouter } from "../trpc";
+import { adminProcedure, createTRPCRouter, publicProcedure } from "../trpc";
 
 export const exchangeRouter = createTRPCRouter({
   add: adminProcedure
@@ -23,5 +23,47 @@ export const exchangeRouter = createTRPCRouter({
           message: "An Error occured. Please try again",
         });
       return exchangeRate;
+    }),
+  getLatestRates: publicProcedure.query(async ({ ctx }) => {
+    const rates = await ctx.prisma.exchangeRate.findMany({
+      include: {
+        Rate: {
+          select: {
+            value: true,
+          },
+          orderBy: {
+            timestamp: "asc",
+          },
+          take: 1,
+        },
+      },
+    });
+    if (!rates)
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "No rates found. Try again",
+      });
+    return rates;
+  }),
+  getLatestRate: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const rates = await ctx.prisma.exchangeRate.findUniqueOrThrow({
+        where: {
+          id: input.id,
+        },
+        include: {
+          Rate: {
+            select: {
+              value: true,
+            },
+            orderBy: {
+              timestamp: "asc",
+            },
+            take: 1,
+          },
+        },
+      });
+      return rates;
     }),
 });
