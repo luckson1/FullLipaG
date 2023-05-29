b96981a0f922a368367a593c3a61e3d65ac62274
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-29 17:27:41 +0300

    fix: add profile apis

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,15 +1,14 @@
 import { accountRouter } from "./router/account";
-import { authRouter } from "./router/auth";
 import { exchangeRouter } from "./router/exchange";
 import { paymentRouter } from "./router/payment";
 import { rateRouter } from "./router/rate";
 import { recipientROuter } from "./router/recipients";
 import { statusRouter } from "./router/status";
 import { transactionRouter } from "./router/transaction";
+import { profileRouter } from "./router/userData";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
-  auth: authRouter,
   transaction: transactionRouter,
   status: statusRouter,
   recipient: recipientROuter,
@@ -17,6 +16,7 @@ export const appRouter = createTRPCRouter({
   exchange: exchangeRouter,
   rate: rateRouter,
   account: accountRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
