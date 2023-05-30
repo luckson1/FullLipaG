a18603c6c0c314b6bb03bbe4509d4861c6e03249
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-30 03:41:01 +0300

    fix: build

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,23 +1,24 @@
 import type { ExpoConfig } from "@expo/config";
 
-const SUPABASE_URL = "https://yowdtkcnjpfigaxbmdee.supabase.co";
+const SUPABASE_URL = "https://ilumelhuwpidacuaztdt.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlvd2R0a2NuanBmaWdheGJtZGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ2MTY4NDYsImV4cCI6MjAwMDE5Mjg0Nn0.unx5UbypypelsoJSZWsISK_h-bBJlf83jVGLlZ72_EQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsdW1lbGh1d3BpZGFjdWF6dGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU0MDA0MDMsImV4cCI6MjAwMDk3NjQwM30.PTQM0xqZg5c3zAYNy9VOcdFrJHhvEeXjDW7BA8pO9i4";
 
 if (typeof SUPABASE_URL !== "string" || typeof SUPABASE_ANON_KEY !== "string") {
   throw new Error("Missing Supabase URL or anonymous key");
 }
 
 const defineConfig = (): ExpoConfig => ({
-  name: "expo",
-  slug: "expo",
+  name: "sino",
+  slug: "sino",
   scheme: "expo",
+  owner: "jagand",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/icon.png",
+  icon: "./assets/app-icon.png",
   userInterfaceStyle: "dark",
   splash: {
-    image: "./assets/icon.png",
+    image: "./assets/app-icon.png",
     resizeMode: "contain",
     backgroundColor: "#18181A",
   },
@@ -30,14 +31,15 @@ const defineConfig = (): ExpoConfig => ({
     bundleIdentifier: "your.bundle.identifier",
   },
   android: {
+    package: "com.sinoremit",
     adaptiveIcon: {
-      foregroundImage: "./assets/icon.png",
+      foregroundImage: "./assets/app-icon.png",
       backgroundColor: "#18181A",
     },
   },
   extra: {
     eas: {
-      projectId: "your-project-id",
+      projectId: "11f431cb-7866-41f7-bec3-d04ed1c32c93",
     },
     SUPABASE_URL,
     SUPABASE_ANON_KEY,
