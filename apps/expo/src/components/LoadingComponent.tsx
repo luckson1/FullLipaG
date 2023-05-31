d051f8e14735aa48be1e9a114fd0a46be6d213d2
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-05-31 12:51:15 +0300

    fix: better loading UI

--- apps/expo/src/components/LoadingComponent.tsx
+++ apps/expo/src/components/LoadingComponent.tsx
@@ -5,15 +5,13 @@ import { SvgUri } from "react-native-svg";
 const LoadingComponent = () => {
   return (
     <View className="h-[60%] w-full ">
-      <View className="my-5 flex h-full w-full items-center justify-center">
+      <View className="my-5 flex h-full w-full items-center justify-center bg-teal-50 opacity-20">
         <SvgUri
           width="70%"
           height="70%"
           uri="https://res.cloudinary.com/dhciks96e/image/upload/v1685356475/undraw_loading_re_5axr_yv9xce.svg"
         />
-        <Text className=" my-5 text-center text-2xl text-white">
-          Loading .....
-        </Text>
+        <Text className=" my-5 text-center text-2xl">Loading .....</Text>
       </View>
     </View>
   );
