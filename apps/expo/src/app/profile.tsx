5ae0aa39f6b1b5570e664e29d2f240e28f2c47b8
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-02 16:12:13 +0300

    fix: separate editing function

--- apps/expo/src/app/profile.tsx
+++ apps/expo/src/app/profile.tsx
@@ -11,7 +11,7 @@ import { DatePickerInput } from "react-native-paper-dates";
 import Toast from "react-native-root-toast";
 import { Stack, useRouter } from "expo-router";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CheckIcon, ScrollView, Select } from "native-base";
+import { CheckIcon, Select } from "native-base";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -37,24 +37,7 @@ const ProfileCreationScreen = () => {
   } = useForm<Profile>({
     resolver: zodResolver(profileSchema),
   });
-  const {
-    data: profile,
-    isLoading,
-    isError,
-  } = api.profile.getUserProfile.useQuery(undefined, {
-    onError(error) {
-      Toast.show(error.message, {
-        duration: Toast.durations.SHORT,
-        position: Toast.positions.TOP,
-        shadow: true,
-        animation: true,
-        backgroundColor: "white",
-        hideOnPress: true,
-        textColor: "red",
-        delay: 0,
-      });
-    },
-  });
+
   const { mutate: createProfile, isLoading: isProfileCreationLoading } =
     api.profile.create.useMutation({
       onError(error) {
@@ -70,25 +53,10 @@ const ProfileCreationScreen = () => {
         });
       },
     });
-  const { mutate: editProfile, isLoading: isProfileEditingLoading } =
-    api.profile.edit.useMutation({
-      onError(error) {
-        Toast.show(error.message, {
-          duration: Toast.durations.SHORT,
-          position: Toast.positions.TOP,
-          shadow: true,
-          animation: true,
-          backgroundColor: "white",
-          hideOnPress: true,
-          textColor: "red",
-          delay: 0,
-        });
-      },
-    });
+
   const onSubmit = (data: Profile) => {
     // Handle form submission
-    const id = profile?.id;
-    id && profile ? editProfile({ ...data, id }) : createProfile(data);
+    createProfile(data);
     router.push("/home");
   };
   return (
@@ -100,164 +68,154 @@ const ProfileCreationScreen = () => {
           headerStyle: { backgroundColor: "rgb(20 184 166) " },
         }}
       />
-      {isLoading && (
-        <View className="h-full w-full">
-          <LoadingComponent />
+
+      <View className="flex w-full flex-1 items-center justify-between bg-teal-500 ">
+        <View className="flex w-full flex-[15%] items-start justify-start">
+          <Text className="mx-5 my-2 text-start text-xl font-semibold text-slate-200">
+            Tell us about yourself
+          </Text>
         </View>
-      )}
-      {!isLoading && !isError && (
-        <View className="flex w-full flex-1 items-center justify-between bg-teal-500 ">
-          <View className="flex w-full flex-[15%] items-start justify-start">
-            <Text className="mx-5 my-2 text-start text-xl font-semibold text-slate-200">
-              Tell us about yourself
-            </Text>
-          </View>
-          <View className="w-full flex-[75%] items-center justify-center bg-white p-2">
-            <KeyboardAvoidingView
-              behavior={Platform.OS === "ios" ? "padding" : "height"}
-              keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}
-              className=" bg-red-5 absolute -top-12 flex w-full rounded-3xl bg-white p-5 shadow"
-            >
-              <Controller
-                control={control}
-                name="firstName"
-                render={({ field }) => (
-                  <TextInput
-                    onChangeText={field.onChange}
-                    onBlur={field.onBlur}
-                    className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
-                    placeholder="First Name"
-                    autoCapitalize="none"
-                  />
-                )}
-              />
-              {errors.firstName && (
-                <Text className="mt-2 text-red-400">
-                  {errors.firstName.message}
-                </Text>
+        <View className="w-full flex-[75%] items-center justify-center bg-white p-2">
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 0}
+            className=" bg-red-5 absolute -top-12 flex w-full rounded-3xl bg-white p-5 shadow"
+          >
+            <Controller
+              control={control}
+              name="firstName"
+              render={({ field }) => (
+                <TextInput
+                  onChangeText={field.onChange}
+                  onBlur={field.onBlur}
+                  className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
+                  placeholder="First Name"
+                  autoCapitalize="none"
+                />
               )}
-
-              <Controller
-                control={control}
-                name="lastName"
-                render={({ field }) => (
-                  <TextInput
-                    onChangeText={field.onChange}
-                    onBlur={field.onBlur}
-                    className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
-                    placeholder="Last Name"
-                    autoCapitalize="none"
-                  />
-                )}
-              />
-              <Controller
-                control={control}
-                name="dateOfBirth"
-                render={({ field }) => (
-                  <DatePickerInput
-                    className="my-2 w-full  rounded-md border border-slate-300  bg-white px-4 text-lg focus:border-teal-500"
-                    locale="en-GB"
-                    label="Date of Birth"
-                    value={field.value}
-                    onChange={field.onChange}
-                    onBlur={field.onBlur}
-                    inputMode="start"
-                  />
-                )}
-              />
-
-              {errors.dateOfBirth && (
-                <Text className="mt-2 text-red-400">
-                  {errors.dateOfBirth.message}
-                </Text>
-              )}
-              {errors.lastName && (
-                <Text className="mt-2 text-red-400">
-                  {errors.lastName.message}
-                </Text>
-              )}
-              <Controller
-                control={control}
-                name="email"
-                render={({ field }) => (
-                  <TextInput
-                    onChangeText={field.onChange}
-                    onBlur={field.onBlur}
-                    className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
-                    placeholder="Email"
-                    autoCapitalize="none"
-                  />
-                )}
-              />
-              {errors.email && (
-                <Text className="mt-2 text-red-400">
-                  {errors.email.message}
-                </Text>
-              )}
-
-              <Controller
-                control={control}
-                name="governmentId"
-                render={({ field }) => (
-                  <TextInput
-                    keyboardType="numeric"
-                    onChangeText={field.onChange}
-                    onBlur={field.onBlur}
-                    className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
-                    placeholder="Government ID"
-                    autoCapitalize="none"
-                  />
-                )}
-              />
-              {errors.governmentId && (
-                <Text className="mt-2 text-red-400">
-                  {errors.governmentId.message}
-                </Text>
-              )}
-              <Controller
-                control={control}
-                name="gender"
-                render={({ field: { onChange, value } }) => (
-                  <Select
-                    selectedValue={value}
-                    width="100%"
-                    className="block w-full  rounded-lg  border-gray-300 px-4 py-3.5 text-base"
-                    accessibilityLabel="Choose Gender"
-                    placeholder="Choose Gender"
-                    _selectedItem={{
-                      bg: "green.400",
-
-                      endIcon: <CheckIcon size="25" />,
-                    }}
-                    mt={1}
-                    onValueChange={(itemValue) => {
-                      onChange(itemValue);
-                    }}
-                  >
-                    <Select.Item label=" Male" value={"Male"} />
-                    <Select.Item label=" Female" value={"Female"} />
-                  </Select>
-                )}
-              />
-            </KeyboardAvoidingView>
-          </View>
-          <View className="flex w-full flex-[10%] items-center justify-end bg-white px-7 py-2 ">
-            <TouchableOpacity
-              className="absolute bottom-2 my-2   flex w-full items-center justify-center rounded-lg bg-green-400 px-4 py-3 shadow-xl"
-              onPress={handleSubmit(onSubmit)}
-            >
-              <Text
-                className="text-xl font-bold text-white"
-                disabled={isProfileCreationLoading || isProfileEditingLoading}
-              >
-                {isProfileCreationLoading || isProfileEditingLoading
-                  ? "Loading"
-                  : "Save"}
+            />
+            {errors.firstName && (
+              <Text className="mt-2 text-red-400">
+                {errors.firstName.message}
               </Text>
-            </TouchableOpacity>
-          </View>
+            )}
+
+            <Controller
+              control={control}
+              name="lastName"
+              render={({ field }) => (
+                <TextInput
+                  onChangeText={field.onChange}
+                  onBlur={field.onBlur}
+                  className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
+                  placeholder="Last Name"
+                  autoCapitalize="none"
+                />
+              )}
+            />
+            <Controller
+              control={control}
+              name="dateOfBirth"
+              render={({ field }) => (
+                <DatePickerInput
+                  className="my-2 w-full  rounded-md border border-slate-300  bg-white px-4 text-lg focus:border-teal-500"
+                  locale="en-GB"
+                  label="Date of Birth"
+                  value={field.value}
+                  onChange={field.onChange}
+                  onBlur={field.onBlur}
+                  inputMode="start"
+                />
+              )}
+            />
+
+            {errors.dateOfBirth && (
+              <Text className="mt-2 text-red-400">
+                {errors.dateOfBirth.message}
+              </Text>
+            )}
+            {errors.lastName && (
+              <Text className="mt-2 text-red-400">
+                {errors.lastName.message}
+              </Text>
+            )}
+            <Controller
+              control={control}
+              name="email"
+              render={({ field }) => (
+                <TextInput
+                  onChangeText={field.onChange}
+                  onBlur={field.onBlur}
+                  className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
+                  placeholder="Email"
+                  autoCapitalize="none"
+                />
+              )}
+            />
+            {errors.email && (
+              <Text className="mt-2 text-red-400">{errors.email.message}</Text>
+            )}
+
+            <Controller
+              control={control}
+              name="governmentId"
+              render={({ field }) => (
+                <TextInput
+                  keyboardType="numeric"
+                  onChangeText={field.onChange}
+                  onBlur={field.onBlur}
+                  className="my-2 w-full  rounded-md border border-slate-300 px-4 py-3 text-lg focus:border-teal-500"
+                  placeholder="Government ID"
+                  autoCapitalize="none"
+                />
+              )}
+            />
+            {errors.governmentId && (
+              <Text className="mt-2 text-red-400">
+                {errors.governmentId.message}
+              </Text>
+            )}
+            <Controller
+              control={control}
+              name="gender"
+              render={({ field: { onChange, value } }) => (
+                <Select
+                  selectedValue={value}
+                  width="100%"
+                  className="block w-full  rounded-lg  border-gray-300 px-4 py-3.5 text-base"
+                  accessibilityLabel="Choose Gender"
+                  placeholder="Choose Gender"
+                  _selectedItem={{
+                    bg: "green.400",
+
+                    endIcon: <CheckIcon size="25" />,
+                  }}
+                  mt={1}
+                  onValueChange={(itemValue) => {
+                    onChange(itemValue);
+                  }}
+                >
+                  <Select.Item label=" Male" value={"Male"} />
+                  <Select.Item label=" Female" value={"Female"} />
+                </Select>
+              )}
+            />
+          </KeyboardAvoidingView>
         </View>
-      )}
+        <View className="flex w-full flex-[10%] items-center justify-end bg-white px-7 py-2 ">
+          <TouchableOpacity
+            className="absolute bottom-2 my-2   flex w-full items-center justify-center rounded-lg bg-green-400 px-4 py-3 shadow-xl"
+            onPress={handleSubmit(onSubmit)}
+          >
+            <Text
+              className="text-xl font-bold text-white"
+              disabled={isProfileCreationLoading}
+            >
+              {isProfileCreationLoading ? "Loading" : "Save"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
