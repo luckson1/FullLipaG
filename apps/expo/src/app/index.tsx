6fbce6e80c5b64a20f530a2e3d7befb374a6d22b
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-07 03:10:46 +0300

    fix: new logic for entry

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,14 +1,35 @@
-import React from "react";
-import { Redirect } from "expo-router";
+import React, { useEffect } from "react";
+import { View } from "react-native";
+import {
+  SplashScreen,
+  useRootNavigationState,
+  useRouter,
+  useSegments,
+} from "expo-router";
 import { useSession } from "@supabase/auth-helpers-react";
+
+import LoadingComponent from "~/components/LoadingComponent";
 
 const Index = () => {
   const session = useSession();
+  const segments = useSegments();
+  const router = useRouter();
+  const navigationState = useRootNavigationState();
+  const isPublicPage = segments[0] === "(public)";
+  const isAuthenticated = session !== null;
+  useEffect(() => {
+    if (!navigationState?.key) return;
+
+    if (!isPublicPage && !isAuthenticated) {
+      router.replace("/onboarding");
+    } else if (isAuthenticated) {
+      router.replace("/home");
+    }
+  }, [isAuthenticated, isPublicPage, navigationState?.key]);
   return (
-    <>
-      {session && <Redirect href={"/home"} />}
-      {!session && <Redirect href={"/onboarding"} />}
-    </>
+    <View className="flex-1">
+      {navigationState?.key ? <SplashScreen /> : <LoadingComponent />}
+    </View>
   );
 };
 
