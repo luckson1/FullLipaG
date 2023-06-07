82818770c5bf58b331c1d679ee531915b6fc7b66
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-07 03:12:16 +0300

    fix layout

--- apps/expo/src/app/(tabs)/recipients/_layout.tsx
+++ apps/expo/src/app/(tabs)/recipients/_layout.tsx
@@ -4,7 +4,16 @@ import { Stack } from "expo-router";
 const _layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          // Set the presentation mode to modal for our modal route.
+          presentation: "modal",
+          headerStyle: { backgroundColor: "rgb(20 184 166)" },
+          headerTitleStyle: { color: "#fff" },
+          headerTitle: " Who will you be sending money to?",
+        }}
+      />
       <Stack.Screen
         name="add"
         options={{
@@ -16,6 +25,15 @@ const _layout = () => {
       />
       <Stack.Screen
         name="edit"
+        options={{
+          // Set the presentation mode to modal for our modal route.
+          presentation: "modal",
+          headerStyle: { backgroundColor: "rgb(20 184 166)" },
+          headerTitleStyle: { color: "#fff" },
+        }}
+      />
+      <Stack.Screen
+        name="[id]"
         options={{
           // Set the presentation mode to modal for our modal route.
           presentation: "modal",
