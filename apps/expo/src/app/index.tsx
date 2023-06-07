e2e62f62141085dc30ee6411529824727e36dc8d
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-08 01:18:20 +0300

    feat: introduce loading state

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
 import { View } from "react-native";
+import Toast from "react-native-root-toast";
 import {
   SplashScreen,
   useRootNavigationState,
   useRouter,
   useSegments,
 } from "expo-router";
-import { useSession } from "@supabase/auth-helpers-react";
+import { useSessionContext } from "@supabase/auth-helpers-react";
 
 import LoadingComponent from "~/components/LoadingComponent";
 
 const Index = () => {
-  const session = useSession();
+  const { session, isLoading, error } = useSessionContext();
+  const errorMessage = error?.message;
   const segments = useSegments();
   const router = useRouter();
   const navigationState = useRootNavigationState();
@@ -19,16 +21,38 @@ const Index = () => {
   const isAuthenticated = session !== null;
   useEffect(() => {
     if (!navigationState?.key) return;
-
-    if (!isPublicPage && !isAuthenticated) {
+    if (errorMessage) {
+      Toast.show(errorMessage, {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.TOP,
+        shadow: true,
+        animation: true,
+        backgroundColor: "white",
+        hideOnPress: true,
+        textColor: "red",
+        delay: 0,
+      });
+    }
+    if (!isPublicPage && !isAuthenticated && !isLoading) {
       router.replace("/onboarding");
     } else if (isAuthenticated) {
       router.replace("/home");
     }
-  }, [isAuthenticated, isPublicPage, navigationState?.key]);
+  }, [
+    isAuthenticated,
+    isPublicPage,
+    navigationState?.key,
+    router,
+    isLoading,
+    errorMessage,
+  ]);
   return (
     <View className="flex-1">
-      {navigationState?.key ? <SplashScreen /> : <LoadingComponent />}
+      {navigationState?.key || isLoading ? (
+        <SplashScreen />
+      ) : (
+        <LoadingComponent />
+      )}
     </View>
   );
 };
