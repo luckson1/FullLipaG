1b8315e25ed36ec793c646344de700d15a832f28
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-16 14:41:42 +0300

    feat: add shadcn ui

--- apps/nextjs/src/pages/signin.tsx
+++ apps/nextjs/src/pages/signin.tsx
@@ -1,90 +1,100 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
-import { Eye, EyeOff, Github } from "lucide-react";
 
-export default function SigninPage() {
+import { Icons } from "~/components/Icons";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { cn } from "~/lib/utils";
+
+type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [isLoading, setIsLoading] = useState(false);
   const supabase = useSupabaseClient();
 
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [isSignUp, setIsSignUp] = useState(false);
 
-  const signInWithPassword = async () => {
-    const { error, data } = isSignUp
-      ? await supabase.auth.signUp({
-          email,
-          password,
-        })
-      : await supabase.auth.signInWithPassword({
-          email,
-          password,
-        });
-    if (error) alert(error.message);
-    else if (isSignUp && data.user) {
-      alert("Check your email for a confirmation link.");
-      setIsSignUp(false);
+  const signInWithGoogle = async () => {
+    setIsLoading(true);
+    const { error, data } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    if (error) {
+      alert(error.message);
+      setIsLoading(false);
+    } else if (data.url) {
+      setIsLoading(false);
+      alert("Check your email for a login magic link.");
     }
   };
 
-  const signInWithGithub = async () => {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: "github",
-      options: { scopes: "read:user user:email" },
+  const signInWithOTP = async (email: string) => {
+    setIsLoading(true);
+    const { error, data } = await supabase.auth.signInWithOtp({
+      email,
+      options: { emailRedirectTo: "/dashboard" },
     });
-    if (error) alert(error.message);
+    if (error) {
+      alert(error.message);
+      setIsLoading(false);
+    }
+    if (data.user) setIsLoading(false);
   };
-
   return (
-    <main className="flex h-screen bg-zinc-900 text-zinc-200">
-      <div className="mx-auto flex flex-col items-center justify-center gap-6">
-        <h1 className="text-3xl font-extrabold tracking-tight">Sign In</h1>
-        <div className="flex w-full flex-col gap-2">
-          <input
-            className="rounded-lg bg-white/10 px-4 py-1 text-zinc-200 transition hover:bg-white/20"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-          />
-          <div className="relative">
-            <input
-              className="w-full rounded-lg bg-white/10 px-4 py-1 text-zinc-200 transition hover:bg-white/20"
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={() => signInWithOTP(email)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              // disabled={isLoading}
             />
-            <button
-              className="absolute bottom-0 right-0 top-0 flex items-center justify-center px-2"
-              onClick={() => setShowPassword((s) => !s)}
-            >
-              {showPassword ? <EyeOff /> : <Eye />}
-            </button>
           </div>
-          <button
-            className="w-full rounded-lg bg-emerald-400 py-2 font-semibold text-zinc-900 no-underline transition hover:bg-emerald-500"
-            onClick={signInWithPassword}
+          <Button
+          // disabled={isLoading}
           >
-            {isSignUp ? "Sign Up" : "Sign In"}
-          </button>
-          <button onClick={() => setIsSignUp((s) => !s)}>
-            {isSignUp ? "Already have an account?" : "Don't have an account?"}
-          </button>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Sign In with Email
+          </Button>
         </div>
-
-        <div className="relative flex w-full justify-center border-b border-zinc-200 py-2">
-          <span className="absolute top-1 bg-zinc-900 px-2">or</span>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
         </div>
-
-        <button
-          className="flex items-center gap-1 rounded-lg bg-white/10 px-10 py-2 font-semibold text-zinc-200 no-underline transition hover:bg-white/20"
-          onClick={signInWithGithub}
-        >
-          <Github size={20} />
-          Continue with Github
-        </button>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background text-muted-foreground px-2">
+            Or continue with
+          </span>
+        </div>
       </div>
-    </main>
+      <Button
+        variant="outline"
+        type="button"
+        onClick={signInWithGoogle}
+        // disabled={isLoading}
+      >
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.google className="mr-2 h-4 w-4" />
+        )}{" "}
+        Google
+      </Button>
+    </div>
   );
 }
