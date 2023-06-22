749dbde91ac519a27b773123a01603e529c734a5
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-22 21:01:06 +0300

    fix: graph

--- apps/nextjs/src/pages/dashboard.tsx
+++ apps/nextjs/src/pages/dashboard.tsx
@@ -52,20 +52,17 @@ import { toast } from "~/components/ui/use-toast";
 import { cn } from "~/lib/utils";
 
 export function RecentSales() {
-  const {
-    data: recentTransactions,
-    isLoading,
-    isError,
-  } = api.transaction.getRecentTransactions.useQuery(undefined, {
-    onError(err) {
-      toast({
-        variant: "destructive",
-        title: "Uh oh! Something went wrong.",
-        description: ` There was a problem: ${err.message}`,
-        action: <ToastAction altText="Try again">Try again</ToastAction>,
-      });
-    },
-  });
+  const { data: recentTransactions, isLoading } =
+    api.transaction.getRecentTransactions.useQuery(undefined, {
+      onError(err) {
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: ` There was a problem: ${err.message}`,
+          action: <ToastAction altText="Try again">Try again</ToastAction>,
+        });
+      },
+    });
   if (isLoading || !recentTransactions)
     return (
       <div className="space-y-8">
@@ -108,63 +105,13 @@ export function RecentSales() {
   );
 }
 
-const data = [
-  {
-    name: "Jan",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Feb",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Mar",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Apr",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "May",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Jun",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Jul",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Aug",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Sep",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Oct",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Nov",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-  {
-    name: "Dec",
-    total: Math.floor(Math.random() * 5000) + 1000,
-  },
-];
-
 export function Overview() {
+  const { data } = api.transaction.getSuccessfulTransactionsPerMonth.useQuery();
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={data}>
         <XAxis
-          dataKey="name"
+          dataKey="month"
           stroke="#888888"
           fontSize={12}
           tickLine={false}
@@ -177,7 +124,7 @@ export function Overview() {
           axisLine={false}
           tickFormatter={(value) => `$${value}`}
         />
-        <Bar dataKey="total" fill="#adfa1d" radius={[4, 4, 0, 0]} />
+        <Bar dataKey="total" fill="rgb(20 184 166)" radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   );
