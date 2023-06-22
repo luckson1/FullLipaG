d56cc608325899551aecccbd1f3351dcbe8af144
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-22 14:55:19 +0300

    fix:copy

--- apps/nextjs/src/pages/dashboard.tsx
+++ apps/nextjs/src/pages/dashboard.tsx
@@ -381,7 +381,7 @@ export default function DashboardPage() {
                 <Card>
                   <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                     <CardTitle className="text-sm font-medium">
-                      Total Payments
+                      Total Transactions
                     </CardTitle>
                     <DollarSign className="text-muted-foreground h-4 w-4" />
                   </CardHeader>
