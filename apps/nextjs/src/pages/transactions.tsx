6acdcfa4b033ec1b3065caaf8247c0a0cef35799
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-24 00:49:48 +0300

    fix: filter  status

--- apps/nextjs/src/pages/transactions.tsx
+++ apps/nextjs/src/pages/transactions.tsx
@@ -254,10 +254,10 @@ export function DataTable({ data }: { data: Transaction[] }) {
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter status..."
+          value={(table.getColumn("status")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn("status")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
