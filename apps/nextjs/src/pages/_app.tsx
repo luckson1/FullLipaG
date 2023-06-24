5edfdc66629b18dff6b967287a9f7d78722a71e4
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-24 03:12:20 +0300

    viewport

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -10,7 +10,7 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 import { api } from "~/utils/api";
 import { ThemeProvider } from "~/components/theme-provider";
-import { Toast, ToastProvider } from "~/components/ui/toast";
+import { Toast, ToastProvider, ToastViewport } from "~/components/ui/toast";
 
 function MyApp({
   Component,
@@ -27,6 +27,7 @@ function MyApp({
         <ToastProvider>
           <Component {...pageProps} />
           <Toast />
+          <ToastViewport />
         </ToastProvider>
       </ThemeProvider>
     </SessionContextProvider>
