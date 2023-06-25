68073cdcd1c228f96f023a8218cba31205d260f9
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2023-06-25 18:04:20 +0300

    chores: comments

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -29,7 +29,6 @@ const getBaseUrl = () => {
    */
   const localhost = Constants.manifest?.debuggerHost?.split(":")[0];
   if (!localhost) {
-    // return "https://your-production-url.com";
     return "https://sinoremit.vercel.app";
     throw new Error(
       "Failed to get localhost. Please point to your production server.",
